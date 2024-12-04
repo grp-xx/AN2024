36499d74995b0f4545a5f9ea1721d57c9cb1a005
“Gregorio <gregorio.procissi@unipi.it>
2024-12-04 09:38:00 +0100

    UDPclt

--- src/UDPclt.cpp
+++ src/UDPclt.cpp
@@ -1,7 +1,9 @@
+#include <cstdlib>
 #include <socket.hpp>
 #include <sockaddress.hpp>
 #include <string>
 #include <iostream>
+#include <sys/socket.h>
 
 int main(int argc, char* argv[])
 {
@@ -9,6 +11,27 @@ int main(int argc, char* argv[])
         std::cout << "Usage: " << argv[0] << " <server> <port>" << std::endl;
         return (1);
     }
+
+    npl::socket<AF_INET, SOCK_DGRAM> sock;
+    npl::sockaddress<AF_INET> srv_addr(argv[1],std::atoi(argv[2]));
+
+    std::string line;
+    std::getline(std::cin, line);
+
+    npl::buffer buff(line.begin(),line.end());
+
+    sock.sendto(buff, srv_addr);
+
+    auto [ricevuto, remoto] = sock.recvfrom(80);
+
+    std::cout << "Received text from: " << remoto.host() << " Port: " << remoto.port() << std::endl;
+
+    std::cout << "Testo: " << std::string(ricevuto.begin(),ricevuto.end()) << std::endl;
+
+    sock.close();
+
+
+
 
 
     return EXIT_SUCCESS;
